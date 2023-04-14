e4a36e0d763920908bb04a3bc2c34f950dfe410d
Christine <christineng1028@gmail.com>
2023-04-14 14:58:15 -0400

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -44,7 +44,7 @@ const Footer = ({ classes }) => {
         </div>
       </Grid>
       <Grid container xs={4} justifyContent="center">
-        Made with 💖 by Christine Ng © 2022
+        Made with 💖 by Christine Ng © 2023
       </Grid>
     </Grid>
   );
